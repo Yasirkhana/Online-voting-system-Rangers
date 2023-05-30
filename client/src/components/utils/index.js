e8e0ddc461f6dd6b185963cf3916ffe8c8bfd1f7
yasirkhana <khan100555yasir@gmail.com>
2023-05-30 14:52:37 +0500

    project setup done
    project setup done

---
+++ client/src/components/utils/index.js
@@ -0,0 +1,10 @@
+export const sName = [
+   "KPK",
+   "Sindh",
+   "punjab",
+   "Balochistan"
+  ];
+
+  export const bName = ["ME","CSE","EC","CE","IT","BBA","MBA","MCA","BCA","B Com"];
+
+  export const userType = ["Admin","Voter"];
