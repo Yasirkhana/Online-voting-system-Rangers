cef1159a6e092c50711855950f78d39e917ab6b6
yasirkhana <khan100555yasir@gmail.com>
2023-06-04 23:30:07 +0500

    Making Responsive Design
    Making Responsive Design

--- client/src/components/screens/Posts.js
+++ client/src/components/screens/Posts.js
@@ -1,48 +1,33 @@
-import React,{useState,useEffect} from 'react'
-import {url} from '../utils/Url'
+import React, { useState, useEffect } from 'react';
+import { url } from '../utils/Url';
+import './Style.css';
 
+const Posts = () => {
+  const [data, setData] = useState([]);
 
+  // Fetch all posts from the server and set the data using the result
+  useEffect(() => {
+    fetch(url + 'posts', {})
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result.posts);
+      });
+  }, []);
 
-const Posts  = ()=>{
-    const [data,setData] = useState([])
-  
-    // Fetch all posts from the server and set the data using the result
-    useEffect(()=>{
-       fetch(url+'posts',{
-         
-       }).then(res=>res.json())
-       .then(result=>{
-         
-           setData(result.posts)
-       })
-    },[])
+  const renderData = data.map((item) => (
+    <div className="posts" key={item.title}>
+      <p>
+        <b>{item && item.title && item.title.split(' ')[0]}</b>
+      </p>
+      <img src={item.photo} alt={item.title} />
+    </div>
+  ));
 
+  return (
+    <div className="home">
+      <div>{renderData}</div>
+    </div>
+  );
+};
 
-   
-   const  renderData = data.map((item) => (
-        <div style={{width:"207px",height:"200px",border:"1px solid grey",marginLeft:"10px",
-        float:"left",marginBottom:"15px",background:"white",borderRadius:"15px"}}  key={item.title}>
-       
-           
-       <p style={{textAlign: "center"}}>
-    <b>{item && item.title && item.title.split(" ")[0]}</b>
-        </p>
-            <img src={item.photo} style={{height:"130px",width:"140px",marginLeft:"31px",borderRadius:"10%"}} />
-                 
-            {/* <p style={{textAlign:"center"}}><b >{item.votes.length > 1 ?  item.votes.length  + " votes" :  + item.votes.length + " vote" }</b></p> */}
-            </div>
-        
-     
-    ))
-  
-   return (
-       <div className="home">
-           <div  style={{width:"450px",height:"500px"}}>
-           {renderData}
-           </div>
-       </div>   
-   )   
-}
-
-
-export default Posts
+export default Posts;
--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -1,123 +1,155 @@
-import React,{useState,useContext,} from 'react'
-import {Link,useHistory} from 'react-router-dom'
-import {userType} from '../utils'
-import {UserContext} from '../../App'
-import Posts from "./Posts";
-import M from 'materialize-css'
-import Image from "../../Image/Capture.JPG"
-import {url} from '../utils/Url'
+import React, { useState, useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { userType } from '../utils';
+import { UserContext } from '../../App';
+import Posts from './Posts';
+import M from 'materialize-css';
+import Image from '../../Image/Capture.JPG';
+import { url } from '../utils/Url';
 
+const SignIn = () => {
+  const { state, dispatch } = useContext(UserContext);
+  const history = useHistory();
+  const [password, setPasword] = useState('');
+  const [email, setEmail] = useState('');
+  const [userItem, setUserItem] = useState('');
 
-
-const SignIn  = ()=>{
-    const {state,dispatch} = useContext(UserContext)
-    const history = useHistory()
-    const [password,setPasword] = useState("")
-    const [email,setEmail] = useState("")
-    const[userItem,setuserItem]=useState("")
-    const PostData = ()=>{
-        //checks the validity of the email entered by the user
-        if(!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
-            M.toast({html: "invalid email",classes:"#c62828 red darken-3"})
-            return
-        }
-        //handling the sign-in functionality and managing the authentication state 
-        fetch(url+"signin",{
-            method:"post",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify({
-                password,
-                email
-            })
-        }).then(res=>res.json())
-        .then(data=>{
-           
-           if(data.error){
-              M.toast({html: data.error,classes:"#c62828 red darken-3"})
-           }
-           else{
-               localStorage.setItem("jwt",data.token)
-               localStorage.setItem("user",JSON.stringify(data.user))
-               dispatch({type:"USER",payload:data.user})
-             
-               history.push('/')
-           }
-        }).catch(err=>{
-            console.log(err)
-        })
+  const PostData = () => {
+    // checks the validity of the email entered by the user
+    if (
+      !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+      )
+    ) {
+      M.toast({
+        html: 'Invalid email',
+        classes: '#c62828 red darken-3',
+      });
+      return;
     }
-   return (
-      <div className="mycard" style={{width:"60%",margin:"auto",paddingTop:"5%"}}>
-          <p><b style={{marginLeft:"5%",fontSize:"24px"}}></b></p> 
-          <div className="row">
-          <div className="col-12">   
-          < div className="clo-6" style={{float:"left"}}>
-             
-           <Posts  />
-           </div>
-          <div className="col-6" style={{float:"right",width:"42%"}}>   
-          <div className="card " style={{paddingLeft:"40px",paddingRight:"40px",paddingTop:"20px",border:"1px solid blue"}}>
-            
-             <img src={Image} alt="imahe" style={{height:"90px",width:"90px",borderRadius:"50%",
-             alignSelf:"center",marginBottom:"15px"}} />
-            
-            <div >
-            <div className="col">
-            <input
-            type="text"
-            placeholder="email"
-            value={email}
-           className='form-control'
-           style={{height:"50px",width:"80%",marginLeft:"10%"}}
-            onChange={(e)=>setEmail(e.target.value)}
-            />
-            <br />
-            </div>
-            <div className="col">
-            <input
-            type="password"
-            placeholder="password"
-            value={password}
-            style={{height:"50px",width:"80%",marginLeft:"10%"}}
-            onChange={(e)=>setPasword(e.target.value)}
-            className='form-control'
-            />
-            </div>
-            <br />
 
-            <div className="col">
-            
-            <select  className="form-select" value={userItem}onChange={(e)=> setuserItem(e.target.value)}
-              style={{height:"50px",width:"80%",marginLeft:"10%"}}
-            >
-            {userType.map((item) => (
-                  <option value={item} key={item} >
-                    {" "}
-                    {item}{" "}
-                  </option>
-                ))}
-            </select>
+    // handling the sign-in functionality and managing the authentication state
+    fetch(url + 'signin', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        password,
+        email,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          M.toast({
+            html: data.error,
+            classes: '#c62828 red darken-3',
+          });
+        } else {
+          localStorage.setItem('jwt', data.token);
+          localStorage.setItem('user', JSON.stringify(data.user));
+          dispatch({ type: 'USER', payload: data.user });
+          history.push('/');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="mycard" style={{ width: '80%', margin: '20px', paddingTop: '5%' }}>
+      <p>
+        <b style={{ marginLeft: '5%', fontSize: '24px' }}></b>
+      </p>
+      <div className="row">
+        <div className="col-md-6 ">
+          <Posts />
         </div>
+        <div className="col-md-6">
+          <div
+            className="card"
+            style={{
+              paddingLeft: '40px',
+              paddingRight: '40px',
+              paddingTop: '20px',
+              border: '1px solid blue',
+            }}
+          >
+            <img
+              src={Image}
+              alt="image"
+              style={{
+                height: '90px',
+                width: '90px',
+                borderRadius: '50%',
+                alignSelf: 'center',
+                marginBottom: '15px',
+              }}
+            />
 
-          </div>
-      
-            <button className="btn btn-success mt-4 "
-             style={{height:"50px",width:"80%",marginLeft:"10%"}}
-            onClick={()=>PostData()}
+            <div>
+              <div className="col">
+                <input
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  className="form-control"
+                  style={{ height: '50px', width: '100%' }}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="col">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  style={{ height: '50px', width: '100%' }}
+                  onChange={(e) => setPasword(e.target.value)}
+                  className="form-control"
+                />
+              </div>
+
+              <div className="col">
+                <select
+                  className="form-select"
+                  value={userItem}
+                  onChange={(e) => setUserItem(e.target.value)}
+                  style={{ height: '50px', width: '100%' }}
+                >
+                  {userType.map((item) => (
+                    <option value={item} key={item}>
+                      {item}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            <button
+              className="btn btn-success mt-4"
+              style={{ height: '50px', width: '100%' }}
+              onClick={PostData}
             >
-                Login
+              Login
             </button>
-            <p style={{fontFamily:"Raleway",textAlign:"center",fontSize:"17px",fontWeight:"500",marginTop:"20px"}}>
-               Not Register ? <Link to="/signup" style={{color:"grey",fontWeight:"700"}}>Register here</Link>
+            <p
+              style={{
+                fontFamily: 'Raleway',
+                textAlign: 'center',
+                fontSize: '17px',
+                fontWeight: '500',
+                marginTop: '20px',
+              }}
+            >
+              Not registered? <Link to="/signup">Register here</Link>
             </p>
+          </div>
         </div>
-        </div>
-      
       </div>
-      </div>
-      </div>
-   )
-}
-export default SignIn
+    </div>
+  );
+};
+
+export default SignIn;
